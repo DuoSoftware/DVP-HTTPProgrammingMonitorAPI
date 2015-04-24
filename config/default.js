72b3ab1d8bba2f8509aae4d2ad8f0fd619151873
Pawan Sasanka <pawan@duosoftware.com>
2015-04-24 16:01:18 +0530

    config changed

--- config/default.js
+++ config/default.js
@@ -1,29 +1,4 @@
 module.exports = {
-    Freeswitch : {
-        ip: 'localhost',
-        port: 8021,
-        password: 'devadmin'
-    },
-
-    WebAPI : {
-        domain: '192.168.1.58',
-        port: 80,
-        path: '/CSRequestWebApi/api/'
-    },
-
-    Redis : {
-        ip: 'localhost',
-        port: 6379
-    },
-
-    HTTPServer : {
-        port: 8085
-    },
-
-
-    LBServer : {
-        path: 'http://localhost:8086/'
-    },
 
     "DB": {
         "Type":"postgres",
