7c5ff1607088528c282a165b5b2794c92d3007da
Pawan Sasanka <pawan@duosoftware.com>
2015-10-13 15:43:54 +0530

    Update ErrorSaving.js

--- ErrorSaving.js
+++ ErrorSaving.js
@@ -4,8 +4,8 @@
 
 var restify = require('restify');
 var stringify=require('stringify');
-var DbConn = require('DVP-DBModels');
-var logger = require('DVP-Common/LogHandler/CommonLogHandler.js').logger;
+var DbConn = require('dvp-dbmodels');
+var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
 
 
 function SaveErrors(ErrObj,callback)
